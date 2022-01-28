e9c75d62604bfe2ac94a42e951267f555b93a8d6
MathFerreira01 <ferreiramatheus124@gmail.com>
2022-01-27 23:36:44 -0300

    first commit

---
+++ src/components/Page/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div `
+.colored:nth-child(1) {
+background: #57FAF7;
+}
+.colored:nth-child(2) {
+background: #00F0B6;
+}
+.colored:nth-child(3) {
+background: #54F0B6;
+}
+.colored:nth-child(4) {
+background: #57FAEC;
+}
+.colored:nth-child(5) {
+background: #5BCAE3;
+}
+.colored:nth-child(6) {
+background: #57B1FA;
+}
+.colored:nth-child(7) {
+background: #5481F0;
+}
+`
+export const Spacer = styled.div `
+    height: 15vh;
+    background: #5481F0;
+`
